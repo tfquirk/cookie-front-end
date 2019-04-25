0538da9276eeb5083dfcedc62299016139d3c738
tfquirk <timothyfquirk@gmail.com>
2019-04-25 09:29:26 -0400

    added inital cases to types and removed generic css files

---
+++ src/Types/Types.js
@@ -0,0 +1,6 @@
+// abstracted types to be used with a dispatch action
+
+// constants for types
+export const ADD_RECIPES = "ADD_RECIPES";
+export const ADD_INGREDIENTS = "ADD_INGREDIENTS";
+export const ADD_RECIPE_INGREDIENTS = "ADD_RECIPE_INGREDIENTS";
