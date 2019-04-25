b5d00f73b9fa9a9897463287fe9c1f794d662e8e
tfquirk <timothyfquirk@gmail.com>
2019-04-25 09:42:11 -0400

    Constants for API endpoints created

---
+++ src/Apis/Endpoints.js
@@ -0,0 +1,11 @@
+// create constants for api endpoints
+
+//Recipes
+export const RECIPES_ENDPOINT = "http://localhost:8000/api/v1/recipes";
+
+//Ingredients
+export const INGREDIENTS_ENDPOINT = "http://localhost:8000/api/v1/ingredients";
+
+//RecipeIngredients join
+export const RECIPE_INGREDIENTS_ENDPOINT =
+  "http://localhost:8000/api/v1/recipe_ingredients";
