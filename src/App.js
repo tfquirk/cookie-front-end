ce41e5f5df11d78ff739cfc1646f81a3ee9273a9
tfquirk <timothyfquirk@gmail.com>
2019-04-28 19:03:50 -0400

    first route added to App '/'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 
 // connect to Redux state
 import { connect } from "react-redux";
 
+// import ability to use React Router funcitionality
+import { Route, Switch } from "react-router-dom";
+
 // import methods for API calls
 import { fetchRecipes } from "./Apis/FetchRecipes";
+
+//pages
+import Homepage from "./Pages/Homepage";
 
 const App = props => {
   useEffect(() => {
@@ -12,7 +18,11 @@ const App = props => {
     props.fetchRecipes();
   });
 
-  return <div className="App">This is a test.</div>;
+  return (
+    <Fragment>
+      <Route exact path="/" component={Homepage} />
+    </Fragment>
+  );
 };
 
 const mapStateToProps = state => {
