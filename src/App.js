c38220b75cce96530264e65f6586d24077f25bcf
tfquirk <timothyfquirk@gmail.com>
2019-04-28 15:24:03 -0400

    added redux connect, and set mapStateToProps and mapDispatchToProps to test API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function App() {
+// connect to Redux state
+import { connect } from "react-redux";
+
+// import methods for API calls
+import { fetchRecipes } from "./Apis/FetchRecipes";
+
+const App = props => {
+  useEffect(() => {
+    //code here
+    props.fetchRecipes();
+  });
+
   return <div className="App">This is a test.</div>;
-}
+};
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    recipes: state.recipes
+  };
+};
+
+// each time a user visits this page, add it to the log history
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchRecipes: () => fetchRecipes(dispatch)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
